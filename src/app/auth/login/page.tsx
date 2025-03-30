86898043b5836e4c1bdb500ea8f3a9501e5767b7
bgocumlu <bgocumlu@users.noreply.github.com>
2025-03-31 00:46:15 +0300

    small fix

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -67,7 +67,7 @@ export default function LoginPage() {
               <ArrowRight className="ml-2 h-4 w-4" />
             </Button>
             <div className="text-center text-sm">
-              Don't have an account?{" "}
+              Don&apos;t have an account?{" "}
               <Link href="/auth/signup" className="text-primary hover:underline">
                 Sign up
               </Link>
